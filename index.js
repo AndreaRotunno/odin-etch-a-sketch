f44d4375cf3e7b8147135640e18cbc32b05f7bea
Andrea Rotunno <andrea.rotunno@outlook.it>
2024-07-21 10:06:27 +0200

    add black coloring on grid hovering

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ for (let i = 0; i < size; i++) {
   row.classList.add("row");
   for (let j = 0; j < size; j++) {
     let block = document.createElement("div");
-    block.classList.add("block");
+    block.classList.add("block", "new");
     row.appendChild(block);
   }
   container.appendChild(row);
 }
+let blocks = document.querySelectorAll(".block");
+blocks.forEach((element) =>
+  element.addEventListener("mouseover", (e) => {
+    e.target.classList.add("hovered");
+    e.target.classList.remove("new");
+  })
+);
