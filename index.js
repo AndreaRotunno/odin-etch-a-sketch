57c95a0f7b39dcd867f4d41334af453c602e59f0
Andrea Rotunno <andrea.rotunno@outlook.it>
2024-07-21 10:19:41 +0200

    add size and reset buttons

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const container = document.querySelector(".container");
-let size = 3;
 
-for (let i = 0; i < size; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < size; j++) {
-    let block = document.createElement("div");
-    block.classList.add("block", "new");
-    row.appendChild(block);
+function createGrid(size) {
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < size; j++) {
+      let block = document.createElement("div");
+      block.classList.add("block", "new");
+      row.appendChild(block);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
+  let blocks = document.querySelectorAll(".block");
+  blocks.forEach((element) =>
+    element.addEventListener("mouseover", (e) => {
+      e.target.classList.add("hovered");
+      e.target.classList.remove("new");
+    })
+  );
 }
-let blocks = document.querySelectorAll(".block");
-blocks.forEach((element) =>
-  element.addEventListener("mouseover", (e) => {
-    e.target.classList.add("hovered");
-    e.target.classList.remove("new");
-  })
-);
+document.querySelector("#size-btn").addEventListener("click", () => {
+  let input = prompt("Enter grid size");
+  createGrid(input);
+});
+document.querySelector("#reset-btn").addEventListener("click", () => {
+  let blocks = document.querySelectorAll(".block");
+  blocks.forEach((e) => {
+    e.classList.remove("hovered");
+    e.classList.add("new");
+  });
+});
+createGrid(16);
